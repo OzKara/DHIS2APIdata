0e945ed9472ee26a917247b29f74a0c414c6f8da
OzKara <oz_247@hotmail.com>
2021-10-07 03:56:42 +0200

    first commit

---
+++ dhis2-api-data/src/Datasets.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { useState } from "react";
+import { useDataQuery } from '@dhis2/app-runtime';
+import { 
+    CircularLoader, 
+    Menu, 
+    MenuItem,
+    Table,
+    TableBody,
+    TableCell,
+    TableCellHead,
+    TableHead,
+    TableRow,
+    TableRowHead, 
+} from '@dhis2/ui';
+
+const dataQuery = {
+    dataSets: {
+        resource: 'dataSets',
+        params: {
+            fields: [
+                'id', 
+                'displayName', 
+                'created',
+                //dataSetElements[dataElement[id, displayName, created]
+            ],
+            paging: false,
+        },
+    },
+}
+
+
+export function Datasets() {
+    const { loading, error, data } = useDataQuery(dataQuery)
+
+    if (error) {
+        return <span>ERROR: {error.message}</span>
+    }
+
+    if (loading) {
+        return <CircularLoader large />
+    }
+
+    if (data) {
+        console.log(data)
+        console.log(data.dataSets.dataSets)
+        return (
+            <>
+            <Menu>
+                {data.dataSets.dataSets.map(row => {
+                    return (
+                        <>
+                        <MenuItem
+                            key={row.id}
+                            label={row.displayName}
+                        >
+                        </MenuItem>
+                        <Table>
+                            <TableHead>
+                                <TableRowHead>
+                                    <TableCellHead>Display Name</TableCellHead>
+                                    <TableCellHead>ID</TableCellHead>
+                                    <TableCellHead>created</TableCellHead>
+                                </TableRowHead>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow key={row.id}>
+                                    <TableCell>{row.displayName}</TableCell>
+                                    <TableCell>{row.id}</TableCell>
+                                    <TableCell>{row.created}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                        </>
+                    );
+                })}
+            </Menu>
+            </>
+        )
+    }
+}
